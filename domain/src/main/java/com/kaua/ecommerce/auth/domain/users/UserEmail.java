317c1f18998eda32211c7543a842022ecc6e5f55
Kauã Pereira <zkauacodes@hotmail.com>
2024-07-26 18:25:39 -0300

    feat(user-valueobjects): add the name, password and email valueobjects

---
+++ domain/src/main/java/com/kaua/ecommerce/auth/domain/users/UserEmail.java
@@ -0,0 +1,16 @@
+package com.kaua.ecommerce.auth.domain.users;
+
+import com.kaua.ecommerce.lib.domain.ValueObject;
+
+import java.util.regex.Pattern;
+
+public record UserEmail(String value) implements ValueObject {
+
+    private static final String EMAIL_PATTERN = Pattern.compile("^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*\\.[a-zA-Z]{2,5}$")
+            .pattern();
+
+    public UserEmail {
+        this.assertArgumentNotEmpty(value, "email", "should not be empty");
+        this.assertArgumentPattern(value, EMAIL_PATTERN, "email", "should be a valid email address");
+    }
+}
