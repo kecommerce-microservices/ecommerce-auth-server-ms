47c1130bb5663cc93433b90fefed7b68c8848bf4
Kauã Pereira <zkauacodes@hotmail.com>
2024-07-28 08:52:53 -0300

    test(user-details): remove public modifier in test

--- infrastructure/src/test/java/com/kaua/ecommerce/auth/infrastructure/userdetails/UserDetailsServiceImplTest.java
+++ infrastructure/src/test/java/com/kaua/ecommerce/auth/infrastructure/userdetails/UserDetailsServiceImplTest.java
@@ -12,7 +12,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 @IntegrationTest
-public class UserDetailsServiceImplTest {
+class UserDetailsServiceImplTest {
 
     @Autowired
     private UserJpaEntityRepository userJpaEntityRepository;
