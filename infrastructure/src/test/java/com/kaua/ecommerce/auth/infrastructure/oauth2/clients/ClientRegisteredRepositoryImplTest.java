7cb8f309200ba05234ba1fa210e6fdd612de9803
Kauã Pereira <zkauacodes@hotmail.com>
2024-07-30 12:31:13 -0300

    test(oauth-clients): add the clients implementation using jpa and in future cache find responses tests

---
+++ infrastructure/src/test/java/com/kaua/ecommerce/auth/infrastructure/oauth2/clients/ClientRegisteredRepositoryImplTest.java
@@ -0,0 +1,117 @@
+package com.kaua.ecommerce.auth.infrastructure.oauth2.clients;
+
+import com.kaua.ecommerce.auth.infrastructure.DatabaseRepositoryTest;
+import com.kaua.ecommerce.auth.infrastructure.oauth2.clients.persistence.ClientJpaEntityRepository;
+import com.kaua.ecommerce.lib.domain.utils.IdentifierUtils;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
+import org.springframework.security.oauth2.server.authorization.settings.TokenSettings;
+
+import java.time.Duration;
+
+@DatabaseRepositoryTest
+public class ClientRegisteredRepositoryImplTest {
+
+    @Autowired
+    private ClientJpaEntityRepository clientJpaEntityRepository;
+
+    @Autowired
+    private ClientRegisteredRepositoryImpl clientRegisteredRepositoryImpl;
+
+    @Test
+    void givenAValidRegisteredClient_whenCallSave_thenSaveClient() {
+        final var aClient = RegisteredClient.withId(IdentifierUtils.generateNewId())
+                .clientId("client-id")
+                .clientSecret("client-secret")
+                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_JWT)
+                .authorizationGrantType(AuthorizationGrantType.PASSWORD)
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
+                .authorizationGrantType(AuthorizationGrantType.JWT_BEARER)
+                .redirectUri("http://localhost:8080")
+                .scope("read")
+                .scope("write")
+                .clientSettings(ClientSettings
+                        .builder()
+                        .requireProofKey(false)
+                        .requireAuthorizationConsent(false)
+                        .build())
+                .tokenSettings(TokenSettings.builder()
+                        .accessTokenTimeToLive(Duration.ofMinutes(5))
+                        .refreshTokenTimeToLive(Duration.ofDays(1))
+                        .reuseRefreshTokens(false)
+                        .build())
+                .build();
+
+        Assertions.assertEquals(0, this.clientJpaEntityRepository.count());
+
+        Assertions.assertDoesNotThrow(() -> this.clientRegisteredRepositoryImpl.save(aClient));
+
+        Assertions.assertEquals(1, this.clientJpaEntityRepository.count());
+
+        final var clientEntity = this.clientRegisteredRepositoryImpl.findByClientId(aClient.getClientId());
+
+        Assertions.assertNotNull(clientEntity);
+        Assertions.assertEquals(aClient.getClientId(), clientEntity.getClientId());
+        Assertions.assertEquals(aClient.getClientSecret(), clientEntity.getClientSecret());
+        Assertions.assertEquals(aClient.getAuthorizationGrantTypes().size(), clientEntity.getAuthorizationGrantTypes().size());
+        Assertions.assertEquals(aClient.getClientAuthenticationMethods().size(), clientEntity.getClientAuthenticationMethods().size());
+        Assertions.assertEquals(aClient.getRedirectUris().size(), clientEntity.getRedirectUris().size());
+        Assertions.assertEquals(aClient.getScopes().size(), clientEntity.getScopes().size());
+        Assertions.assertEquals(aClient.getClientSettings().isRequireAuthorizationConsent(), clientEntity.getClientSettings().isRequireAuthorizationConsent());
+        Assertions.assertEquals(aClient.getClientSettings().isRequireProofKey(), clientEntity.getClientSettings().isRequireProofKey());
+        Assertions.assertEquals(aClient.getTokenSettings().getAccessTokenTimeToLive(), clientEntity.getTokenSettings().getAccessTokenTimeToLive());
+        Assertions.assertEquals(aClient.getTokenSettings().getRefreshTokenTimeToLive(), clientEntity.getTokenSettings().getRefreshTokenTimeToLive());
+        Assertions.assertEquals(aClient.getTokenSettings().isReuseRefreshTokens(), clientEntity.getTokenSettings().isReuseRefreshTokens());
+    }
+
+    @Test
+    void givenAValidId_whenCallFindById_thenReturnRegisteredClient() {
+        final var aClient = RegisteredClient.withId(IdentifierUtils.generateNewId())
+                .clientId("client-id")
+                .clientSecret("client-secret")
+                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_JWT)
+                .authorizationGrantType(AuthorizationGrantType.PASSWORD)
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
+                .authorizationGrantType(AuthorizationGrantType.JWT_BEARER)
+                .redirectUri("http://localhost:8080")
+                .scope("read")
+                .scope("write")
+                .clientSettings(ClientSettings
+                        .builder()
+                        .requireProofKey(false)
+                        .requireAuthorizationConsent(false)
+                        .build())
+                .tokenSettings(TokenSettings.builder()
+                        .accessTokenTimeToLive(Duration.ofMinutes(5))
+                        .refreshTokenTimeToLive(Duration.ofDays(1))
+                        .reuseRefreshTokens(false)
+                        .build())
+                .build();
+
+        Assertions.assertDoesNotThrow(() -> this.clientRegisteredRepositoryImpl.save(aClient));
+
+        final var clientEntity = this.clientRegisteredRepositoryImpl.findById(aClient.getId());
+
+        Assertions.assertNotNull(clientEntity);
+        Assertions.assertEquals(aClient.getClientId(), clientEntity.getClientId());
+        Assertions.assertEquals(aClient.getClientSecret(), clientEntity.getClientSecret());
+        Assertions.assertEquals(aClient.getAuthorizationGrantTypes().size(), clientEntity.getAuthorizationGrantTypes().size());
+        Assertions.assertEquals(aClient.getClientAuthenticationMethods().size(), clientEntity.getClientAuthenticationMethods().size());
+        Assertions.assertEquals(aClient.getRedirectUris().size(), clientEntity.getRedirectUris().size());
+        Assertions.assertEquals(aClient.getScopes().size(), clientEntity.getScopes().size());
+        Assertions.assertEquals(aClient.getClientSettings().isRequireAuthorizationConsent(), clientEntity.getClientSettings().isRequireAuthorizationConsent());
+        Assertions.assertEquals(aClient.getClientSettings().isRequireProofKey(), clientEntity.getClientSettings().isRequireProofKey());
+        Assertions.assertEquals(aClient.getTokenSettings().getAccessTokenTimeToLive(), clientEntity.getTokenSettings().getAccessTokenTimeToLive());
+        Assertions.assertEquals(aClient.getTokenSettings().getRefreshTokenTimeToLive(), clientEntity.getTokenSettings().getRefreshTokenTimeToLive());
+        Assertions.assertEquals(aClient.getTokenSettings().isReuseRefreshTokens(), clientEntity.getTokenSettings().isReuseRefreshTokens());
+    }
+}
---
+++ infrastructure/src/test/java/com/kaua/ecommerce/auth/infrastructure/oauth2/clients/persistence/ClientJpaEntityRepositoryTest.java
@@ -0,0 +1,98 @@
+package com.kaua.ecommerce.auth.infrastructure.oauth2.clients.persistence;
+
+import com.kaua.ecommerce.auth.infrastructure.DatabaseRepositoryTest;
+import com.kaua.ecommerce.lib.domain.utils.IdentifierUtils;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Set;
+
+@DatabaseRepositoryTest
+public class ClientJpaEntityRepositoryTest {
+
+    @Autowired
+    private ClientJpaEntityRepository clientJpaEntityRepository;
+
+    @Test
+    void givenAValidValues_whenCallSave_thenSaveClient() {
+        final var aClientEntity = new ClientEntity();
+        aClientEntity.setId(IdentifierUtils.generateNewId());
+        aClientEntity.setClientId("client-id");
+        aClientEntity.setSecret("client-secret");
+
+        final var aAuthenticationMethod = new AuthenticationMethodEntity();
+        aAuthenticationMethod.setId(IdentifierUtils.generateNewId());
+        aAuthenticationMethod.setAuthenticationMethod("client-secret-basic");
+        aAuthenticationMethod.setClient(aClientEntity);
+
+        final var aGrantType = new GrantTypeEntity();
+        aGrantType.setId(IdentifierUtils.generateNewId());
+        aGrantType.setGrantType("password");
+        aGrantType.setClient(aClientEntity);
+
+        final var aRedirectUrl = new RedirectUrlEntity();
+        aRedirectUrl.setId(IdentifierUtils.generateNewId());
+        aRedirectUrl.setUrl("http://localhost:8080");
+        aRedirectUrl.setClient(aClientEntity);
+
+        final var aScope = new ScopeEntity();
+        aScope.setId(IdentifierUtils.generateNewId());
+        aScope.setScope("read");
+        aScope.setClient(aClientEntity);
+
+        final var aClientEntitySettings = new ClientSettingsEntity();
+        aClientEntitySettings.setId(IdentifierUtils.generateNewId());
+        aClientEntitySettings.setRequireProofKey(false);
+        aClientEntitySettings.setRequireAuthorizationConsent(false);
+        aClientEntitySettings.setClient(aClientEntity);
+
+        final var aTokenSettings = new ClientTokenSettingsEntity();
+        aTokenSettings.setId(IdentifierUtils.generateNewId());
+        aTokenSettings.setAccessTokenTTL(5);
+        aTokenSettings.setRefreshTokenTTL(1);
+        aTokenSettings.setReuseRefreshTokens(false);
+        aTokenSettings.setClient(aClientEntity);
+
+        aClientEntity.setAuthenticationMethods(Set.of(aAuthenticationMethod));
+        aClientEntity.setGrantTypes(Set.of(aGrantType));
+        aClientEntity.setRedirectUrls(Set.of(aRedirectUrl));
+        aClientEntity.setScopes(Set.of(aScope));
+        aClientEntity.setClientSettings(aClientEntitySettings);
+        aClientEntity.setClientTokenSettings(aTokenSettings);
+
+        Assertions.assertEquals(0, this.clientJpaEntityRepository.count());
+
+        Assertions.assertDoesNotThrow(() -> this.clientJpaEntityRepository.save(aClientEntity));
+
+        Assertions.assertEquals(1, this.clientJpaEntityRepository.count());
+
+        final var clientEntity = this.clientJpaEntityRepository.findByClientId(aClientEntity.getClientId()).get();
+
+        Assertions.assertNotNull(clientEntity);
+        Assertions.assertEquals(aClientEntity.getClientId(), clientEntity.getClientId());
+        Assertions.assertEquals(aClientEntity.getSecret(), clientEntity.getSecret());
+        Assertions.assertEquals(aAuthenticationMethod.getId(), clientEntity.getAuthenticationMethods().iterator().next().getId());
+        Assertions.assertEquals(aAuthenticationMethod.getAuthenticationMethod(), clientEntity.getAuthenticationMethods().iterator().next().getAuthenticationMethod());
+        Assertions.assertEquals(aAuthenticationMethod.getAuthenticationMethod(), clientEntity.getAuthenticationMethods().iterator().next().getAuthenticationMethod());
+        Assertions.assertNotNull(aAuthenticationMethod.getClient());
+        Assertions.assertEquals(aGrantType.getId(), clientEntity.getGrantTypes().iterator().next().getId());
+        Assertions.assertEquals(aGrantType.getGrantType(), clientEntity.getGrantTypes().iterator().next().getGrantType());
+        Assertions.assertNotNull(aGrantType.getClient());
+        Assertions.assertEquals(aRedirectUrl.getId(), clientEntity.getRedirectUrls().iterator().next().getId());
+        Assertions.assertEquals(aRedirectUrl.getUrl(), clientEntity.getRedirectUrls().iterator().next().getUrl());
+        Assertions.assertNotNull(aRedirectUrl.getClient());
+        Assertions.assertEquals(aScope.getId(), clientEntity.getScopes().iterator().next().getId());
+        Assertions.assertEquals(aScope.getScope(), clientEntity.getScopes().iterator().next().getScope());
+        Assertions.assertNotNull(aScope.getClient());
+        Assertions.assertEquals(aClientEntitySettings.getId(), clientEntity.getClientSettings().getId());
+        Assertions.assertEquals(aClientEntitySettings.isRequireAuthorizationConsent(), clientEntity.getClientSettings().isRequireAuthorizationConsent());
+        Assertions.assertEquals(aClientEntitySettings.isRequireProofKey(), clientEntity.getClientSettings().isRequireProofKey());
+        Assertions.assertNotNull(aClientEntitySettings.getClient());
+        Assertions.assertEquals(aTokenSettings.getId(), clientEntity.getClientTokenSettings().getId());
+        Assertions.assertEquals(aTokenSettings.getAccessTokenTTL(), clientEntity.getClientTokenSettings().getAccessTokenTTL());
+        Assertions.assertEquals(aTokenSettings.getRefreshTokenTTL(), clientEntity.getClientTokenSettings().getRefreshTokenTTL());
+        Assertions.assertEquals(aTokenSettings.isReuseRefreshTokens(), clientEntity.getClientTokenSettings().isReuseRefreshTokens());
+        Assertions.assertNotNull(aTokenSettings.getClient());
+    }
+}
