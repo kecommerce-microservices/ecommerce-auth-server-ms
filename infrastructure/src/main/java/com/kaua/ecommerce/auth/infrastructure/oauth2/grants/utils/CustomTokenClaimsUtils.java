313d0a6ff929faa2acf1f2d2c04e3527426b2659
Kauã Pereira <zkauacodes@hotmail.com>
2024-08-03 18:07:39 -0300

    feat(custom-token-claims-utils): add this class to contains custom jwt clains names

---
+++ infrastructure/src/main/java/com/kaua/ecommerce/auth/infrastructure/oauth2/grants/utils/CustomTokenClaimsUtils.java
@@ -0,0 +1,9 @@
+package com.kaua.ecommerce.auth.infrastructure.oauth2.grants.utils;
+
+public final class CustomTokenClaimsUtils {
+
+    private CustomTokenClaimsUtils() {}
+
+    public static final String IS_MICROSERVICE = "is_microservice";
+    public static final String AUTHORITIES = "authorities";
+}
