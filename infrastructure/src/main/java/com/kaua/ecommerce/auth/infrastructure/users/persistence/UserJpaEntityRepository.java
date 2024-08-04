8496a96c0f99cf1adbedbec310a6879a49dd0c18
Kauã Pereira <zkauacodes@hotmail.com>
2024-08-04 16:26:59 -0300

    feat(user-jpa-entity-repository): add the custom query to load roles and user mfa by email using join

--- infrastructure/src/main/java/com/kaua/ecommerce/auth/infrastructure/users/persistence/UserJpaEntityRepository.java
+++ infrastructure/src/main/java/com/kaua/ecommerce/auth/infrastructure/users/persistence/UserJpaEntityRepository.java
@@ -1,6 +1,7 @@
 package com.kaua.ecommerce.auth.infrastructure.users.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 import java.util.UUID;
@@ -9,5 +10,6 @@ public interface UserJpaEntityRepository extends JpaRepository<UserJpaEntity, UU
 
     boolean existsByEmail(String email);
 
+    @Query("SELECT u FROM UserJpaEntity u LEFT JOIN FETCH u.roles LEFT JOIN FETCH u.mfa WHERE u.email = :email")
     Optional<UserJpaEntity> findByEmail(String email);
 }
