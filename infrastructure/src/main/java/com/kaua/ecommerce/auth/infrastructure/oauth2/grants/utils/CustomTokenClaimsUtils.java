8047bd8a6bab0905f82518f35864f050d5946679
Kauã Pereira <zkauacodes@hotmail.com>
2024-08-04 16:40:24 -0300

    feat(custom-token-claims-utils): add the all authorities role constant

--- infrastructure/src/main/java/com/kaua/ecommerce/auth/infrastructure/oauth2/grants/utils/CustomTokenClaimsUtils.java
+++ infrastructure/src/main/java/com/kaua/ecommerce/auth/infrastructure/oauth2/grants/utils/CustomTokenClaimsUtils.java
@@ -6,4 +6,6 @@ public final class CustomTokenClaimsUtils {
 
     public static final String IS_MICROSERVICE = "is_microservice";
     public static final String AUTHORITIES = "authorities";
+
+    public static final String ALL_AUTHORITIES = "*";
 }
