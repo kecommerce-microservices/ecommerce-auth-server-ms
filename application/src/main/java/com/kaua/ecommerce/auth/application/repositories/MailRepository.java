a07a76db1aa059757eba861fb01acc3d3500d60c
Kauã Pereira <zkauacodes@hotmail.com>
2024-08-07 15:28:43 -0300

    feat(mail-repository): add the find by token method

--- application/src/main/java/com/kaua/ecommerce/auth/application/repositories/MailRepository.java
+++ application/src/main/java/com/kaua/ecommerce/auth/application/repositories/MailRepository.java
@@ -3,12 +3,17 @@ package com.kaua.ecommerce.auth.application.repositories;
 import com.kaua.ecommerce.auth.domain.mailtokens.MailToken;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface MailRepository {
 
     MailToken save(MailToken mailToken);
 
+    MailToken update(MailToken mailToken);
+
     List<MailToken> findByEmail(String email);
+
+    Optional<MailToken> findByToken(String token);
 
     void deleteByToken(String token);
 }
