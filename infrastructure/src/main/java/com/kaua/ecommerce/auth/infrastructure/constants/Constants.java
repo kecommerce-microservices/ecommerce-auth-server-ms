9ad75cea89bac54f3564e48ec58b88cd3dfdba2a
Kauã Pereira <zkauacodes@hotmail.com>
2024-08-08 16:40:58 -0300

    feat(constants): add the jwt key constants

--- infrastructure/src/main/java/com/kaua/ecommerce/auth/infrastructure/constants/Constants.java
+++ infrastructure/src/main/java/com/kaua/ecommerce/auth/infrastructure/constants/Constants.java
@@ -6,4 +6,7 @@ public final class Constants {
 
     public static final String MFA_PUBLIC_KEY = "mfa-public.key";
     public static final String MFA_PRIVATE_KEY = "mfa-private.key";
+
+    public static final String JWT_PUBLIC_KEY = "jwt-public.key";
+    public static final String JWT_PRIVATE_KEY = "jwt-private.key";
 }
