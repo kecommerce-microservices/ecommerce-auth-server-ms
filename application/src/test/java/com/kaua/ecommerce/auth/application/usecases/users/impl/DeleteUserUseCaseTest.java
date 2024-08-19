8667ac38d7cc314e779d9a5f81ad77181bfd929a
Kauã Pereira <zkauacodes@hotmail.com>
2024-08-19 08:23:52 -0300

    fix(sonar): fix sonar issues

--- application/src/test/java/com/kaua/ecommerce/auth/application/usecases/users/impl/DeleteUserUseCaseTest.java
+++ application/src/test/java/com/kaua/ecommerce/auth/application/usecases/users/impl/DeleteUserUseCaseTest.java
@@ -11,7 +11,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 
-public class DeleteUserUseCaseTest extends UseCaseTest {
+class DeleteUserUseCaseTest extends UseCaseTest {
 
     @Mock
     private UserRepository userRepository;
