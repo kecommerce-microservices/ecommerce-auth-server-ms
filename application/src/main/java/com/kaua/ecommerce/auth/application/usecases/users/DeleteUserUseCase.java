ef7ec348d15caba018c17753a671266fba02d7c2
Kauã Pereira <zkauacodes@hotmail.com>
2024-08-19 08:18:46 -0300

    feat(delete-user-usecase): add the delete user usecase abstraction and implementation

---
+++ application/src/main/java/com/kaua/ecommerce/auth/application/usecases/users/DeleteUserUseCase.java
@@ -0,0 +1,7 @@
+package com.kaua.ecommerce.auth.application.usecases.users;
+
+import com.kaua.ecommerce.auth.application.UnitUseCase;
+import com.kaua.ecommerce.auth.domain.users.UserId;
+
+public abstract class DeleteUserUseCase extends UnitUseCase<UserId> {
+}
